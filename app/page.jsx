28d8c99ab800c519319468c2332ad36f61bb7dfc
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-12-19 21:26:17 +0000

    added interactive video component

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import InteractiveVideoBot from "./components/InteractiveVideoBot";
+
+export default function Home() {
+  return (
+    <div className="w-11/12 m-auto flex-col my-6">
+      <InteractiveVideoBot />
+    </div>
+  );
+}
