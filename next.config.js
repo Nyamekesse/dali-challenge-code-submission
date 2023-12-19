2741d905d733d859ff56a113aaeafffcf143fd67
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-12-19 17:07:04 +0000

    setup next-config.js to use environment variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    PINECONE_API_KEY: process.env.PINECONE_API_KEY,
+    PINECONE_ENVIRONMENT: process.env.PINECONE_ENVIRONMENT,
+    PINECONE_INDEX: process.env.PINECONE_INDEX,
+    SERPAPI_API_KEY: process.env.SERPAPI_API_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
